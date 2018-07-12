551fd39b87cffa4769146609ee13ec7c3f8225a4
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-07-12 11:18:26 -0700

    setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// app.get('/', (req, res) {
 
+// });
+app.use(express.static('./public'));
+
+app.listen(3000, ()=>{
+  console.log('LISTENING TO PORT 3000');
+})
